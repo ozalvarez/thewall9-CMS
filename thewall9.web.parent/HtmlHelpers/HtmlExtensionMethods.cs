00a3d623f59b66ec38d77f7a6a3b7ab9cf651453
ozalvarez <oswalalvarez@gmail.com>
2015-06-01 10:36:15 -0500

    Google Analytics New Features

--- thewall9.web.parent/HtmlHelpers/HtmlExtensionMethods.cs
+++ thewall9.web.parent/HtmlHelpers/HtmlExtensionMethods.cs
@@ -25,6 +25,22 @@ namespace thewall9.web.parent.HtmlHelpers
     </script>";
             return new MvcHtmlString(_Script.ToString());
         }
+        public static MvcHtmlString GAScript_QAQ(this HtmlHelper helper)
+        {
+            var _Script = @" <script>
+var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '"+APP._Site.Site.GAID+@"']);
+    _gaq.push(['_trackPageview']);
+
+    (function () {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        //ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    </script>";
+            return new MvcHtmlString(_Script.ToString());
+        }
         //DELETE ME
         public static MvcHtmlString Lang(this HtmlHelper helper)
         {
