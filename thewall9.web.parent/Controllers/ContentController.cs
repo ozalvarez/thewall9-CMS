9967da126021f03aed94c7c56eec2298b57a0b50
ozalvarez <oswalalvarez@gmail.com>
2015-08-17 13:49:27 -0500

    Add Content Controller

---
+++ thewall9.web.parent/Controllers/ContentController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using thewall9.web.parent.BLL;
+using thewall9.web.parent.HtmlHelpers;
+
+namespace thewall9.web.parent.Controllers
+{
+    [Route("content")]
+    public class ContentController : Controller
+    {
+        ContentBLL _ContentService = new ContentBLL();
+
+        public PartialViewResult Index(string Alias, string View)
+        {
+            return PartialView(View,_ContentService.Get(Request.Url.Authority, Alias));
+        }
+    }
+}
