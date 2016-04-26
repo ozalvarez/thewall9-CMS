0db55299baadeded9233c9e6cfde0ecf09c37d79
Oswaldo Alvarez <oswalalvarez@gmail.com>
2016-04-26 10:46:43 -0500

    SYNC UPDATES

--- thewall9.bll/PageBLL.cs
+++ thewall9.bll/PageBLL.cs
@@ -217,11 +217,11 @@ namespace thewall9.bll
             {
                 var _Q = SiteID != 0
                     ? from p in _c.PageCultures
-                      where p.Page.SiteID == SiteID && p.Published
+                      where p.Page.SiteID == SiteID && p.Published && p.Culture.Name.Equals(Lang)
                       select p
                      : from m in _c.PageCultures
                        join u in _c.SiteUrls on m.Page.Site.SiteID equals u.SiteID
-                       where u.Url.Equals(Url) && !m.Page.InMenu && m.Published
+                       where u.Url.Equals(Url) && !m.Page.InMenu && m.Published && m.Culture.Name.Equals(Lang)
                        select m;
                 return OrderPages(_Q.ToList(), 0);
             }
